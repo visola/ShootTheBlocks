ca5cbbb970a1e6c09d90eb0aa5ff4eee6239e50a
Vinicius Isola <viniciusisola@gmail.com>
2015-01-30 23:51:35 -0500

    Change background to white

--- js/scene.js
+++ js/scene.js
@@ -1,18 +1,10 @@
-var camera, floor, light, renderer, scene;
+var camera, light, renderer, scene;
 
 scene = new Physijs.Scene;
 
 camera = new THREE.PerspectiveCamera(35, w / h, 1, 1000);
 camera.lookAt( scene.position );
 scene.add( camera );
-
-floor = new Physijs.BoxMesh(
-  new THREE.BoxGeometry(100, 1, 100),
-  createMaterial(0xDDDDDD,0.3,1),
-  0
-);
-floor.receiveShadow = true;
-scene.add(floor);
 
 light = new THREE.DirectionalLight(0xffffff);
 light.position.set(45, 50, 45);
@@ -24,6 +16,7 @@ scene.add(light);
 window.addEventListener("load", function () {
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(w, h);
+  renderer.setClearColor( 0xffffff );
   renderer.shadowMapEnabled = true;
   renderer.shadowMapType = THREE.PCFSoftShadowMap;
   document.getElementById( 'viewport' ).appendChild( renderer.domElement );
