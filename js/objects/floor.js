78c86b41fd048fdb4b1e2a585f781cf3284b74c8
Vinicius Isola <viniciusisola@gmail.com>
2015-02-14 10:00:50 -0500

    Change floor friction

--- js/objects/floor.js
+++ js/objects/floor.js
@@ -7,7 +7,7 @@
   
   var floorMesh = floor.mesh = new Physijs.BoxMesh(
     new THREE.BoxGeometry(floor.length, floor.height, floor.width),
-    game.functions.createMaterial(0xDDDDDD,0.3,1),
+    game.functions.createMaterial(0xDDDDDD,0.8,1),
     0
   );
 
